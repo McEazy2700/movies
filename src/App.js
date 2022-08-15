65d1629380157715c9062e47f597920c1e982d18
Ezekiel Victor <102771161+McEazy2700@users.noreply.github.com>
2022-08-15 16:45:51 +0100

    changed routes again

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <Navigation setSearch={setSearch}/>
       <Routes>
         <Route exact={true} path='/' element={<Movies search={search}/>} />
-        <Route path='/:movieId' element={<MovieDetail />} />
+        <Route exact path='/movie/:movieId' element={<MovieDetail />} />
       </Routes>
     </BrowserRouter>
   );
